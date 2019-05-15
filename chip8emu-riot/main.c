3ffbe09dbfd16d936ad9ad8722fda1198e89947c
thaolt <thaolt@songphi.com>
2019-05-16 05:18:05 +0700

    working sdcard mount && oled i2c monitor

--- chip8emu-riot/main.c
+++ chip8emu-riot/main.c
@@ -1,14 +1,58 @@
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <inttypes.h>
 
+#include "pthread.h"
+
+#ifndef NATIVE_BOARD
 #include "periph/gpio.h"
 #include "periph/i2c.h"
+#endif /* !NATIVE_BOARD */
+
+#ifdef MODULE_MTD_SDCARD
+#include "mtd_sdcard.h"
+#include "sdcard_spi.h"
+#include "sdcard_spi_params.h"
+#endif
+
+#include "fs/fatfs.h"
+#include "vfs.h"
+#include "mtd.h"
 
 #include "xtimer.h"
 #include "u8g2.h"
 
-/**
- * @brief   RIOT-OS logo, 64x32 pixels at 8 pixels per byte.
- */
+#include "shell_commands.h"
+#include "shell.h"
+
+
+#if FATFS_FFCONF_OPT_FS_NORTC == 0
+#include "periph/rtc.h"
+#endif
+
+#define SDCARD_MOUNT_POINT "/sdcard"
+
+static fatfs_desc_t fatfs = {
+    .vol_idx = 0
+};
+
+static vfs_mount_t sdcard_vfs_mount = {
+    .mount_point = SDCARD_MOUNT_POINT,
+    .fs = &fatfs_file_system,
+    .private_data = (void *)&fatfs,
+};
+
+#define SDCARD_SPI_NUM (sizeof(sdcard_spi_params) / sizeof(sdcard_spi_params[0]))
+mtd_dev_t *fatfs_mtd_devs[FF_VOLUMES];
+extern sdcard_spi_t sdcard_spi_devs[SDCARD_SPI_NUM];
+mtd_sdcard_t mtd_sdcard_devs[SDCARD_SPI_NUM];
+/* always default to first sdcard*/
+static mtd_dev_t *mtd1 = (mtd_dev_t*)&mtd_sdcard_devs[0];
+
+
+
 static const uint8_t logo[] = {
     0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x03, 0xE0,
     0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0F, 0xF8, 0x00, 0x00, 0x00, 0x00,
@@ -35,43 +79,136 @@ static const uint8_t logo[] = {
 };
 
 #ifndef NATIVE_BOARD
-/**
- * @brief   RIOT-OS pin maping of U8g2 pin numbers to RIOT-OS GPIO pins.
- * @note    To minimize the overhead, you can implement an alternative for
- *          u8x8_gpio_and_delay_riotos.
- */
-static gpio_t pins[] = {
-    [U8X8_PIN_I2C_CLOCK] = GPIO_PIN(PORT_B, 8),
-    [U8X8_PIN_I2C_DATA] = GPIO_PIN(PORT_B, 9),
-    [U8X8_PIN_RESET] = GPIO_PIN(PORT_A, 5)
+    /**
+     * @brief   RIOT-OS pin maping of U8g2 pin numbers to RIOT-OS GPIO pins.
+     * @note    To minimize the overhead, you can implement an alternative for
+     *          u8x8_gpio_and_delay_riotos.
+     */
+    static gpio_t pins[] = {
+        [U8X8_PIN_I2C_CLOCK] = GPIO_PIN(PORT_B, 8),
+        [U8X8_PIN_I2C_DATA] = GPIO_PIN(PORT_B, 9),
+        [U8X8_PIN_RESET] = GPIO_UNDEF
+    };
+
+    /**
+     * @brief   Bit mapping to indicate which pins are set.
+     */
+    static uint32_t pins_enabled = (
+        (1 << U8X8_PIN_I2C_CLOCK) +
+        (1 << U8X8_PIN_I2C_DATA) +
+        (1 << U8X8_PIN_RESET)
+    );
+#endif /* !NATIVE_BOARD */
+
+static int _cat(int argc, char **argv)
+{
+    if (argc < 2) {
+        printf("Usage: %s <file>\n", argv[0]);
+        return 1;
+    }
+    /* With newlib, low-level syscalls are plugged to RIOT vfs
+     * on native, open/read/write/close/... are plugged to RIOT vfs */
+#ifdef MODULE_NEWLIB
+    FILE *f = fopen(argv[1], "r");
+    if (f == NULL) {
+        printf("file %s does not exist\n", argv[1]);
+        return 1;
+    }
+    char c;
+    while (fread(&c, 1, 1, f) != 0) {
+        putchar(c);
+    }
+    fclose(f);
+#else
+    int fd = open(argv[1], O_RDONLY);
+    if (fd < 0) {
+        printf("file %s does not exist\n", argv[1]);
+        return 1;
+    }
+    char c;
+    while (read(fd, &c, 1) != 0) {
+        putchar(c);
+    }
+    close(fd);
+#endif
+    return 0;
+}
+
+static int _tee(int argc, char **argv)
+{
+    if (argc != 3) {
+        printf("Usage: %s <file> <str>\n", argv[0]);
+        return 1;
+    }
+
+#ifdef MODULE_NEWLIB
+    FILE *f = fopen(argv[1], "w+");
+    if (f == NULL) {
+        printf("error while trying to create %s\n", argv[1]);
+        return 1;
+    }
+    if (fwrite(argv[2], 1, strlen(argv[2]), f) != strlen(argv[2])) {
+        puts("Error while writing");
+    }
+    fclose(f);
+#else
+    int fd = open(argv[1], O_RDWR | O_CREAT);
+    if (fd < 0) {
+        printf("error while trying to create %s\n", argv[1]);
+        return 1;
+    }
+    if (write(fd, argv[2], strlen(argv[2])) != (ssize_t)strlen(argv[2])) {
+        puts("Error while writing");
+    }
+    close(fd);
+#endif
+    return 0;
+}
+
+static const shell_command_t shell_commands[] = {
+    { "cat", "print the content of a file", _cat },
+    { "tee", "write a string in a file", _tee },
+    { NULL, NULL, NULL }
 };
 
-/**
- * @brief   Bit mapping to indicate which pins are set.
- */
-static uint32_t pins_enabled = (
-    (1 << U8X8_PIN_I2C_CLOCK) +
-    (1 << U8X8_PIN_I2C_DATA) +
-    (1 << U8X8_PIN_RESET)
-);
+void *shell_thread(void *parameter) {
+    (void) parameter;
+    char line_buf[SHELL_DEFAULT_BUFSIZE];
+    shell_run(shell_commands, line_buf, SHELL_DEFAULT_BUFSIZE);
+    return NULL;
+}
+/** @} */
 
-#endif
 
 int main(void)
 {
+    xtimer_sleep(1);
+    printf("%s\n", "first line on main");
+
+    for(unsigned int i = 0; i < SDCARD_SPI_NUM; i++){
+        mtd_sdcard_devs[i].base.driver = &mtd_sdcard_driver;
+        mtd_sdcard_devs[i].sd_card = &sdcard_spi_devs[i];
+        mtd_sdcard_devs[i].params = &sdcard_spi_params[i];
+        fatfs_mtd_devs[i] = &mtd_sdcard_devs[i].base;
+        mtd_init(&mtd_sdcard_devs[i].base);
+    }
+    fatfs_mtd_devs[fatfs.vol_idx] = mtd1;
+
+    vfs_mount(&sdcard_vfs_mount);
+
     uint32_t screen = 0;
     u8g2_t u8g2;
     
     /* initialize the display */
-    puts("Initializing display.");
+    printf("Initializing display..");
 
 #ifdef NATIVE_BOARD
-    puts("Initializing to SDL.");
+    printf("SDL.\n");
     u8g2_SetupBuffer_SDL_128x64_4(&u8g2, &u8g2_cb_r0);
-    u8x8_InitDisplay(u8g2_GetU8x8(&u8g2));
-    u8x8_SetPowerSave(u8g2_GetU8x8(&u8g2), 0); 
+    u8g2_InitDisplay(&u8g2);
+    u8g2_SetPowerSave(&u8g2, 0);
 #else
-    puts("Initializing to I2C.");
+    printf("I2C.\n");
     // full screen buffer takes 1024 bytes
     u8g2_Setup_sh1106_i2c_128x64_noname_f(&u8g2, U8G2_R0, u8x8_byte_riotos_hw_i2c, u8x8_gpio_and_delay_riotos);
 
@@ -83,9 +220,14 @@ int main(void)
     u8g2_SetPowerSave(&u8g2, 0);
 #endif
 
+    pthread_t thid_shell;
+    pthread_attr_t thattr_shell;
+
+    pthread_attr_init(&thattr_shell);
+    pthread_create(&thid_shell, &thattr_shell, shell_thread, NULL);
 
     /* start drawing in a loop */
-    puts("Drawing on screen.");
+    printf("Drawing on screen.\n");
 
     while (1) {
         u8g2_FirstPage(&u8g2);
